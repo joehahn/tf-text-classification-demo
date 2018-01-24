58444fb201c39a6a260085e52ecff213ebded064
joehahn <jmh.datasciences@gmail.com>
2018-01-24 00:13:49 -0600

    random buys

--- helper_fns.py
+++ helper_fns.py
@@ -8,6 +8,7 @@
 
 #imports used below
 import pandas as pd
+import numpy as np
 
 #read NYSE data into pandas dataframe
 def read_nyse_data(data_path, start_date=None, end_date=None, drop_holidays=None):
@@ -39,4 +40,34 @@ def read_nyse_data(data_path, start_date=None, end_date=None, drop_holidays=None
         df = df[idx]
     return df
 
-
+#random buyers
+def random_purchases(market_data, N_buyers, N_tickers, initial_dollars):
+    random_buys = pd.DataFrame()
+    dates = market_data['date'].unique()
+    for buyer in range(N_buyers):
+        rn_seed = 100 + buyer
+        np.random.seed(seed=rn_seed)
+        current_dollars = initial_dollars
+        dates_dollars = {}
+        for date in dates:
+            idx = (market_data['date'] == date)
+            market_1day = market_data[idx]
+            all_tickers = market_1day['ticker'].unique()
+            selected_tickers = np.random.choice(all_tickers, size=N_tickers, replace=False)
+            idx = market_1day['ticker'].isin(selected_tickers)
+            market_selected = market_1day[idx].sort_values('open', ascending=False)
+            tickers = market_selected['ticker']
+            open_price = market_selected['open']
+            close_price = market_selected['close']
+            dollars_per_ticker = current_dollars/N_tickers
+            N_shares = (dollars_per_ticker/open_price).astype(int)
+            total_share_cost = (N_shares*open_price).sum()
+            cash = current_dollars - total_share_cost
+            total_share_value = (N_shares*close_price).sum()
+            current_dollars = cash + total_share_value
+            dates_dollars[date] = current_dollars
+        random_buys[buyer] = pd.Series(dates_dollars)
+    random_buys = random_buys.sort_index()
+    random_buys['dollars_mean'] = random_buys.mean(axis=1)
+    random_buys['dollars_std'] = random_buys.std(axis=1)/np.sqrt(N_buyers - 1)
+    return random_buys[['dollars_mean', 'dollars_std']]
