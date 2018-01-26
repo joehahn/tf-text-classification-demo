d50ad14db3250244bc62f4d4b56896aa142155d0
joehahn <jmh.datasciences@gmail.com>
2018-01-26 13:46:43 -0600

    parse

--- parse_texts.py
+++ parse_texts.py
@@ -15,10 +15,13 @@ import subprocess
 files = subprocess.check_output(['find', 'iso', '-name', '*.txt']).decode('utf8').split('\n')[0:-1]
 print 'approx number of books downloaded = ', len(files)
 
+#size (in words) of each chunk of text
+chunk_size = 100
+
 #this attempts to extract author & title from a sentence
 def get_author_title(sentence):
-    author = 'unclear'
-    title = 'unclear'
+    author = 'Unclear'
+    title = 'Unclear'
     sentence_lc = sentence.lower()
     if ('gutenberg' in sentence_lc):
         gutenberg_split = sentence_lc.split('project gutenberg')
@@ -30,7 +33,7 @@ def get_author_title(sentence):
         author = author.strip('.')
         author = author.strip(' ')
         author = author.strip('*')
-        title = title.replace('etext of ', '').replace('etext ', '').strip(' ').strip(',')
+        title = title.replace('ebook of ', '').replace('etext of ', '').replace('etext ', '').strip(' ').strip(',')
         if (title[0:3] == "'s "):
             title = title[3:]
         author = author.title()
@@ -44,52 +47,57 @@ import nltk.data
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 import re
 import random
-sentences_list = []
+chunk_list = []
 for file in files:
     try:
         with open(file) as fp:
-            raw_text = fp.read().decode('utf8').replace('\r\n', '|')
+            raw_text = fp.read().decode('utf8')#.replace('\r\n', '|')
         print '===='
         print 'file = ', file
-        if (raw_text[0] == '|'):
-            raw_text = raw_text[1:]
-        for j in range(len(raw_text)):
-            if (raw_text[j] == '|'):
+        first_1000_chars = raw_text[0:1000].replace('\r\n', '|')
+        if (first_1000_chars[0] == '|'):
+            first_1000_chars = first_1000_chars[1:]
+        for j in range(len(first_1000_chars)):
+            if (first_1000_chars[j] == '|'):
                 break
-        sentence = raw_text[0:j]
+        sentence = first_1000_chars[0:j]
         print 'sentence =', sentence
         author, title = get_author_title(sentence)
         print 'author = ', author
         print ' title = ', title
-        modified_text = raw_text.replace('|', ' ')
-        sentences = nltk.sent_tokenize(modified_text)
-        N_sentences = len(sentences)
-        print 'N_sentences = ', N_sentences
+        modified_text = raw_text.replace('\r\n', ' ')
+        words = nltk.word_tokenize(modified_text)
+        N_words = len(words)
+        #print 'N_words = ', N_words
         #drop first and last 15% of sentences that contain gutenberg boilerplate text
-        middle_sentences = sentences[int(0.15*N_sentences) : int(0.85*N_sentences)]
-        #preserve up 1000-3000 random sentences
-        N_sentences = len(middle_sentences)
-        if (N_sentences > 1000):
-            if (N_sentences > 3000):
-                N_sentences = 3000
-        print 'preserving N_sentences = ', N_sentences
-        random_sentences = random.sample(middle_sentences, N_sentences)
-        for j in range(5):
-            print 'sentence ' + str(j) + ' =' + random_sentences[j]
-        for s in random_sentences:
-            sentences_list += [{'input_file':file, 'author':author, 'title':title, 'sentence':s}]
+        middle_words = words[int(0.15*N_words) : int(0.85*N_words)]
+        N_words = len(middle_words)
+        text_chunks = []
+        for j in range(0, N_words, chunk_size):
+            chunk_of_words_list = middle_words[j:j+chunk_size]
+            chunk_of_words_str = ''
+            for word in chunk_of_words_list:
+                chunk_of_words_str += word + ' '
+            text_chunks += [chunk_of_words_str]
+        print 'number of text_chunks = ', len(text_chunks)
+        #insert each chunk into a dict containing author, title, file, chunk etc
+        for text_chunk in text_chunks:
+            chunk_list += [{'input_file':file, 'author':author, 'title':title, 'text_chunk':text_chunk}]
     except:
         pass
 
 #make dataframe of sentences
 import pandas as pd
-sentences = pd.DataFrame(sentences_list)
+cols = ['author', 'title', 'input_file', 'text_chunk']
+chunks = pd.DataFrame(chunk_list)[cols]
+idx = (chunks['author'] != 'Unclear') & (chunks['title'] != 'Unclear')
+chunks = chunks[idx]
 print '===='
 print '===='
-print 'number of parsed sentences = ', len(sentences)
-print 'number of parsed books = ', len(sentences.groupby('input_file').count())
+print 'number of parsed chunks of text = ', len(chunks)
+print 'number of parsed books = ', len(chunks.groupby('input_file').count())
 
 #save sentences
 import pickle
-with open('sentences.pkl', 'wb') as fp:
-    pickle.dump(sentences, fp)
+with open('chunks.pkl', 'wb') as fp:
+    pickle.dump(chunks, fp)
