9ce105d5a467a83f417835651375d1494496118c
joehahn <jmh.datasciences@gmail.com>
2018-01-26 20:24:46 -0600

    parse

--- parse_texts.py
+++ parse_texts.py
@@ -30,7 +30,7 @@ def get_author_title(sentence):
             title = by_split[0]
             author = by_split[1]
         #some cleanup
-        author = author.replace('etext', '').replace("'S", "'s")
+        author = author.replace('etext', '').replace("'S", "'s").replace("complete", "")
         author = author.strip('.')
         author = author.strip(' ')
         author = author.strip('*')
